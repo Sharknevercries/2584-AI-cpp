ef2b40da1a70c1df4473946e20c5d096c80c5bbf
Sharknevercries <sharknevercries@gmail.com>
2017-12-13 13:56:20 +0800

    init

---
+++ 2048.cpp
@@ -0,0 +1,254 @@
+/**
+ * Basic Environment for Game 2048
+ * use 'g++ -std=c++0x -O3 -g -o 2048 2048.cpp' to compile the source
+ *
+ * Computer Games and Intelligence (CGI) Lab, NCTU, Taiwan
+ * http://www.aigames.nctu.edu.tw
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <string>
+#include <random>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <chrono>
+#include <map>
+#include <regex>
+#include <memory>
+#include "board.h"
+#include "action.h"
+#include "agent.h"
+#include "statistic.h"
+
+int shell(int argc, const char* argv[]) {
+
+	struct match {
+		std::string id;
+		std::shared_ptr<agent> play;
+		std::shared_ptr<agent> evil;
+		board b;
+		std::shared_ptr<statistic> stat;
+		match(const std::string& id, const std::string& info) : id(id) {
+			play = std::shared_ptr<agent>(new agent("name=" + info.substr(0, info.find(':')) + " role=dummy"));
+			evil = std::shared_ptr<agent>(new agent("name=" + info.substr(info.find(':') + 1) + " role=dummy"));
+			stat = std::shared_ptr<statistic>(new statistic(1, -1));
+		}
+	};
+
+	std::map<std::string, std::shared_ptr<agent>> lounge;
+	std::map<std::string, std::shared_ptr<match>> arena;
+	bool save_statistics = false;
+	bool debug_mode = false;
+
+	for (int i = 1; i < argc; i++) {
+		std::string para(argv[i]);
+		if (para.find("--play") == 0) {
+			std::string args = para.find("--play=") == 0 ? para.substr(para.find("=") + 1) : "";
+			std::shared_ptr<agent> who(new player(args)); // TODO: change to your player agent
+			lounge[who->name()] = who;
+		} else if (para.find("--evil") == 0) {
+			std::string args = para.find("--evil=") == 0 ? para.substr(para.find("=") + 1) : "";
+			std::shared_ptr<agent> who(new rndenv(args)); // TODO: change to your environment agent
+			lounge[who->name()] = who;
+		} else if (para.find("--save") == 0) {
+			save_statistics = true;
+		} else if (para.find("--debug") == 0) {
+			debug_mode = true;
+		}
+	}
+
+	std::regex match_open  ("^match \\S+ open \\S+:\\S+$");
+	std::regex match_take  ("^match \\S+ (play|evil) take turn$");
+	std::regex match_move  ("^match \\S+ (play|evil) move \\S+$");
+	std::regex match_close ("^match \\S+ close \\S+$");
+
+	std::ostream& dout = debug_mode ? std::cerr : *(new std::ofstream);
+	for (std::string command; std::getline(std::cin, command); ) {
+		dout << "<< " << command << std::endl;
+		try {
+			if (std::regex_match(command, match_take)) {
+				std::string id, role, buf;
+				std::stringstream(command) >> buf >> id >> role;
+
+				if (arena.find(id) == arena.end()) continue;
+				match& m = *arena.at(id);
+				agent& who = *(role == "play" ? m.play : m.evil);
+				if (who.role() == "dummy") continue;
+				action a = who.take_action(m.b);
+
+				std::stringstream oss;
+				oss << "match " << id << " " << role << " move " << std::hex << int(a);
+				std::string out = oss.str();
+				std::cout << out << std::endl;
+				dout << ">> " << out << std::endl;
+
+			} else if (std::regex_match(command, match_move)) {
+				std::string id, role, buf; int code;
+				std::stringstream(command) >> buf >> id >> role >> buf >> std::hex >> code;
+
+				if (arena.find(id) == arena.end()) continue;
+				match& m = *arena.at(id);
+				statistic& stat = *m.stat;
+				int r = action(code).apply(m.b);
+				dout << m.b << "reward = " << r << std::endl;
+				if (r != -1) {
+					stat.save_action(code);
+				}
+
+			} else if (std::regex_match(command, match_open)) {
+				std::string id, info, buf;
+				std::stringstream(command) >> buf >> id >> buf >> info;
+
+				if (arena.find(id) != arena.end()) {
+					dout << id << " is running now" << std::endl;
+					continue;
+				}
+
+				arena[id] = std::shared_ptr<match>(new match(id, info));
+				match& m = *arena.at(id);
+				statistic& stat = *m.stat;
+				stat.open_episode(info);
+				m.b = stat.make_empty_board();
+
+				auto open_episode_for = [&](std::string name) -> std::shared_ptr<agent> {
+					auto it = std::find_if(lounge.begin(), lounge.end(),
+							[=](std::pair<std::string, std::shared_ptr<agent>> p) {
+						return p.second->name() == name;
+					});
+					if (it == lounge.end()) return {};
+					std::string role = it->second->role().find("play") == 0 ? "play" : "evil";
+					std::shared_ptr<agent>& who = role == "play" ? m.play : m.evil;
+					who = it->second;
+					who->open_episode(role == "play" ? "~:" + m.evil->name() : m.play->name() + ":~");
+
+					std::stringstream oss;
+					oss << "match " << id << " " << role << " ready" << std::endl;
+					std::string out = oss.str();
+					std::cout << out << std::endl;
+					dout << ">> " << out << std::endl;
+					return who;
+				};
+
+				bool as_play = open_episode_for(m.play->name()) != nullptr;
+				bool as_evil = open_episode_for(m.evil->name()) != nullptr;
+				if (!(as_play || as_evil)) {
+					dout << info << " not found" << std::endl;
+					arena.erase(id);
+				}
+
+			} else if (std::regex_match(command, match_close)) {
+				std::string id, win, buf;
+				std::stringstream(command) >> buf >> id >> buf >> win;
+
+				if (arena.find(id) == arena.end()) continue;
+				match& m = *arena.at(id);
+				statistic& stat = *m.stat;
+				stat.close_episode(win);
+				m.play->close_episode(win);
+				m.evil->close_episode(win);
+
+				if (save_statistics) {
+					std::string save = id + "-" + win + ".stat";
+					dout << "saving statistic to " << save << "..." << std::endl;
+					std::ofstream out;
+					out.open(save.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
+					if (out.is_open()) {
+						out << stat;
+						out.close();
+					}
+				}
+
+				arena.erase(id);
+			}
+		} catch (std::logic_error&) {
+		}
+
+	}
+
+	return 0;
+}
+
+int main(int argc, const char* argv[]) {
+	std::cout << "2048-Demo: ";
+	std::copy(argv, argv + argc, std::ostream_iterator<const char*>(std::cout, " "));
+	std::cout << std::endl << std::endl;
+
+	size_t total = 1000, block = 0, limit = 0;
+	std::string play_args, evil_args;
+	std::string load, save;
+	bool summary = false;
+	for (int i = 1; i < argc; i++) {
+		std::string para(argv[i]);
+		if (para.find("--total=") == 0) {
+			total = std::stoull(para.substr(para.find("=") + 1));
+		} else if (para.find("--block=") == 0) {
+			block = std::stoull(para.substr(para.find("=") + 1));
+		} else if (para.find("--limit=") == 0) {
+			limit = std::stoull(para.substr(para.find("=") + 1));
+		} else if (para.find("--play=") == 0) {
+			play_args = para.substr(para.find("=") + 1);
+		} else if (para.find("--evil=") == 0) {
+			evil_args = para.substr(para.find("=") + 1);
+		} else if (para.find("--load=") == 0) {
+			load = para.substr(para.find("=") + 1);
+		} else if (para.find("--save=") == 0) {
+			save = para.substr(para.find("=") + 1);
+		} else if (para.find("--summary") == 0) {
+			summary = true;
+		} else if (para.find("--shell") == 0) {
+			return shell(argc, argv);
+		}
+	}
+
+	statistic stat(total, block, limit);
+
+	if (load.size()) {
+		std::ifstream in;
+		in.open(load.c_str(), std::ios::in | std::ios::binary);
+		if (!in.is_open()) return -1;
+		in >> stat;
+		in.close();
+	}
+
+	player play(play_args);
+	rndenv evil(evil_args);
+
+	while (!stat.is_finished()) {
+		play.open_episode("~:" + evil.name());
+		evil.open_episode(play.name() + ":~");
+
+		stat.open_episode(play.name() + ":" + evil.name());
+		board game = stat.make_empty_board();
+		while (true) {
+			agent& who = stat.take_turns(play, evil);
+			action move = who.take_action(game);
+			if (move.apply(game) == -1) break;
+			stat.save_action(move);
+			if (who.check_for_win(game)) break;
+		}
+		agent& win = stat.last_turns(play, evil);
+		stat.close_episode(win.name());
+
+		play.close_episode(win.name());
+		evil.close_episode(win.name());
+	}
+
+	if (summary) {
+		stat.summary();
+	}
+
+	if (save.size()) {
+		std::ofstream out;
+		out.open(save.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
+		if (!out.is_open()) return -1;
+		out << stat;
+		out.flush();
+		out.close();
+	}
+
+	return 0;
+}
---
+++ agent.h
@@ -0,0 +1,152 @@
+#pragma once
+#include <string>
+#include <random>
+#include <sstream>
+#include <map>
+#include <type_traits>
+#include <algorithm>
+#include "board.h"
+#include "action.h"
+#include "weight.h"
+
+class agent {
+public:
+	agent(const std::string& args = "") {
+		std::stringstream ss("name=unknown role=unknown " + args);
+		for (std::string pair; ss >> pair; ) {
+			std::string key = pair.substr(0, pair.find('='));
+			std::string value = pair.substr(pair.find('=') + 1);
+			property[key] = { value };
+		}
+	}
+	virtual ~agent() {}
+	virtual void open_episode(const std::string& flag = "") {}
+	virtual void close_episode(const std::string& flag = "") {}
+	virtual action take_action(const board& b) { return action(); }
+	virtual bool check_for_win(const board& b) { return false; }
+
+public:
+	virtual std::string name() const { return property.at("name"); }
+	virtual std::string role() const { return property.at("role"); }
+	virtual void notify(const std::string& msg) { property[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
+
+protected:
+	typedef std::string key;
+	struct value {
+		std::string value;
+		operator std::string() const { return value; }
+		template<typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
+		operator numeric() const { return numeric(std::stod(value)); }
+	};
+	std::map<key, value> property;
+};
+
+/**
+ * evil (environment agent)
+ * add a new random tile on board, or do nothing if the board is full
+ * 2-tile: 90%
+ * 4-tile: 10%
+ */
+class rndenv : public agent {
+public:
+	rndenv(const std::string& args = "") : agent("name=rndenv role=environment " + args) {
+		if (property.find("seed") != property.end())
+			engine.seed(int(property["seed"]));
+	}
+
+	virtual action take_action(const board& after) {
+		int space[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+		std::shuffle(space, space + 16, engine);
+		for (int pos : space) {
+			if (after(pos) != 0) continue;
+			std::uniform_int_distribution<int> popup(0, 9);
+			int tile = popup(engine) ? 1 : 2;
+			return action::place(tile, pos);
+		}
+		return action();
+	}
+
+private:
+	std::default_random_engine engine;
+};
+
+/**
+ * TODO: player (non-implement)
+ * always return an illegal action
+ */
+class player : public agent {
+public:
+	player(const std::string& args = "") : agent("name=player role=player " + args), alpha(0.0025f) {
+		episode.reserve(32768);
+		if (property.find("seed") != property.end())
+			engine.seed(int(property["seed"]));
+		if (property.find("alpha") != property.end())
+			alpha = float(property["alpha"]);
+
+		if (property.find("load") != property.end())
+			load_weights(property["load"]);
+		// TODO: initialize the n-tuple network
+	}
+	~player() {
+		if (property.find("save") != property.end())
+			save_weights(property["save"]);
+	}
+
+	virtual void open_episode(const std::string& flag = "") {
+		episode.clear();
+		episode.reserve(32768);
+	}
+
+	virtual void close_episode(const std::string& flag = "") {
+		// TODO: train the n-tuple network by TD(0)
+	}
+
+	virtual action take_action(const board& before) {
+		action best;
+		// TODO: select a proper action
+		// TODO: push the step into episode
+		return best;
+	}
+
+public:
+	virtual void load_weights(const std::string& path) {
+		std::ifstream in;
+		in.open(path.c_str(), std::ios::in | std::ios::binary);
+		if (!in.is_open()) std::exit(-1);
+		size_t size;
+		in.read(reinterpret_cast<char*>(&size), sizeof(size));
+		weights.resize(size);
+		for (weight& w : weights)
+			in >> w;
+		in.close();
+	}
+
+	virtual void save_weights(const std::string& path) {
+		std::ofstream out;
+		out.open(path.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
+		if (!out.is_open()) std::exit(-1);
+		size_t size = weights.size();
+		out.write(reinterpret_cast<char*>(&size), sizeof(size));
+		for (weight& w : weights)
+			out << w;
+		out.flush();
+		out.close();
+	}
+
+private:
+	std::vector<weight> weights;
+
+	struct state {
+		// TODO: select the necessary components of a state
+		board before;
+		board after;
+		action move;
+		int reward;
+	};
+
+	std::vector<state> episode;
+	float alpha;
+
+private:
+	std::default_random_engine engine;
+};
