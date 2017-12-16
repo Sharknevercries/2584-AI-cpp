2daac61b8d8901ecd3e92d405273e49b2f4198d7
sharknevercries <sharknevercries@gmail.com>
2017-12-16 16:00:05 +0800

    Env minimax implementation

--- agent.h
+++ agent.h
@@ -52,24 +52,134 @@ protected:
  */
 class rndenv : public agent {
 public:
-	rndenv(const std::string& args = "") : agent("name=rndenv role=environment " + args) {
+	rndenv(const std::string& args = "") : agent("name=rndenv role=environment " + args), alpha(0.0025f), active(true) {
 		if (property.find("seed") != property.end())
 			engine.seed(int(property["seed"]));
+		if (property.find("active") != property.end())
+			active = ((int)property["active"] == 1) ? true : false;
+		if (active) {
+			if (property.find("alpha") != property.end())
+				alpha = float(property["alpha"]);
+
+			tn = tuple_netwrok({
+				{0, 1, 2, 3, 4},
+				{0, 1, 4, 5, 6},
+				{1, 2, 3, 5, 6},
+				{5, 6, 7, 9, 10},
+			});
+
+			if (property.find("load") != property.end())
+				tn.load_weights(property["load"]);
+		}
 	}
 
-	virtual action take_action(const board& after) {
-		std::shuffle(space.begin(), space.end(), engine);
-		for (int pos : space) {
-			if (after(pos) != 0) continue;
-			std::uniform_int_distribution<int> popup(0, 3);
-			int tile = popup(engine) ? 1 : 3;
-			return action::place(tile, pos);
+	~rndenv() {
+		if (property.find("save") != property.end())
+			tn.save_weights(property["save"]);
+	}
+
+	virtual void open_episode(const std::string& flag = "") {
+		if (active) {
+			episode.clear();
+			episode.reserve(32768);
 		}
+	}
+
+	virtual void close_episode(const std::string& flag = "") {
+		if (active) {
+			auto cur = episode.end() - 1, prev = cur - 1;
+			tn.update(cur->before, alpha * (-tn.estimate(cur->before)));		
+			while (cur != episode.begin()) {
+				float td_error = cur->reward + tn.estimate(cur->before) - tn.estimate(prev->before);
+				tn.update(prev->before, alpha * td_error);
+				cur = prev;
+				--prev;
+			}
+		}
+	}
+
+	virtual action take_action(const board& b) {
+		std::uniform_int_distribution<int> popup(0, 3);
+		const int tile = popup(engine) ? 1 : 3;
+
+		if (active) {
+			float max_value = -1e9;
+			int best_pos = -1;
+			board temp1(b), temp2(b);
+			for (const int pos : space) {
+				if (b(pos) != 0) continue;
+				float value = 0;
+				temp1(pos) = 1, temp2(pos) = 3;
+				value += min_node(1, temp1) * 0.75;
+				value += min_node(1, temp2) * 0.25;
+				temp1(pos) = 0, temp2(pos) = 0;
+				if (value > max_value) {
+					max_value = value;
+					best_pos = pos;
+				}
+			}
+
+			episode.push_back(state(b, -1)); // step reward for env
+			return best_pos == -1 ? action() : action::place(tile, best_pos);
+		}
+		else {
+			std::shuffle(space.begin(), space.end(), engine);
+			for (const int pos : space) {
+				if (b(pos) != 0) continue;
+				return action::place(tile, pos);
+			}
+		}
+
 		return action();
+	}
+private:
+	float min_node(const int level, const board& b) {
+		float min_value = 1e9;
+		for (int op = 0; op < 4; ++op) {
+			board temp(b);
+			int reward = action::move(op).apply(temp);
+			float esti = 0;
+			if (reward != -1) {
+				if (level - 2 > 0)
+					esti = max_node(level - 1, temp);
+				else
+					esti = tn.estimate(temp);
+				min_value = std::min(min_value, esti);
+			}
+			else
+				min_value = std::min(min_value, 0.0f);
+		}
+		return min_value;
+	}
+
+	float max_node(const int level, const board& b) {
+		float max_value = -1e9;
+		board temp1(b), temp2(b);
+		for (const int pos : space) {
+			if (b(pos) != 0) continue;
+			float value = 0;
+			temp1(pos) = 1, temp2(pos) = 3;
+			value += min_node(level - 1, temp1) * 0.75;
+			value += min_node(level - 1, temp2) * 0.25;
+			temp1(pos) = 0, temp2(pos) = 0;
+			max_value = std::max(max_value, value);
+		}
+		return max_value;
 	}
 
 private:
+
+	struct state {
+		state(const board& before, int reward) : before(before), reward(reward) {}
+		board before;
+		int reward;
+	};
+
 	std::default_random_engine engine;
+	tuple_netwrok tn;
+	std::vector<state> episode;
+	float alpha;
+	bool active;
 };
 
 /**
@@ -87,6 +197,7 @@ public:
 
 		tn = tuple_netwrok({
 			{0, 1, 2, 3, 4},
+			{0, 1, 4, 5, 6},
 			{1, 2, 3, 5, 6},
 			{5, 6, 7, 9, 10},
 		});
@@ -171,6 +282,7 @@ private:
 			temp1(i) = 1, temp2(i) = 3;
 			v += max_node(level - 1, temp1) / empty_tiles * 0.75;
 			v += max_node(level - 1, temp2) / empty_tiles * 0.25;
+			temp1(i) = 0, temp2(i) = 0;
 		}
 		return v;
 	}
@@ -181,7 +293,6 @@ private:
 	struct state {
 		state() {}
 		state(const board& after, const action& act, int reward) : after(after), move(act), reward(reward) {}
-		// TODO: select the necessary components of a state
 		board after;
 		action move;
 		int reward;
